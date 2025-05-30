585bac99703ce98e4d3043224e80e8dbcff5b9f1
AllieBaig <136852901+AllieBaig@users.noreply.github.com>
2025-05-30 16:24:16 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@
  */
 
 import { applyUIMode } from './utils/uiModeManager.js';
-import { loadUserProfile } from './tools/profileManager.js';
+import { loadUserProfile } from '/tools/profileManager.js';
 import { initVersionDisplay } from './utils/version.js';
 import { updateXPDisplay } from './utils/xpTracker.js';
 import { renderGameMenu } from './utils/menuRenderer.js';
